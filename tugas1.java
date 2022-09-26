2e58e68e53c20bc4464914da9876448fee0ec7f2
ratuurn <114390295+ratuurn@users.noreply.github.com>
2022-09-26 15:11:47 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,9 @@
+public class tugas1 {
+   
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
